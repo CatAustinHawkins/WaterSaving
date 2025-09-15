ecc52eab4397f623cd9b9a6ace6c04985877a839
Cat <angelcah321@hotmail.com>
2025-09-15 22:06:33 +0100

    Notes

---
+++ Assets/Scripts/Expansion.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Expansion : MonoBehaviour
+{
+    /*
+     * Water Quiz can have custom questions
+     * Startup menu for water quiz
+     * Choose number of questions to be made
+     * Choose between multiple choice or input 
+    */
+}
